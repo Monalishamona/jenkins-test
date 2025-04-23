611833157457a3f4d90a0b9518f2f71d17f243d4
Monalisha <121451640+Monalishamona@users.noreply.github.com>
2025-04-23 21:55:55 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -7,4 +7,4 @@ sleep 1
 echo $! > .pidfile
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo 'Visit https://jenkins-test-rzsr.vercel.app/ to see your Node.js/React application in action.'
